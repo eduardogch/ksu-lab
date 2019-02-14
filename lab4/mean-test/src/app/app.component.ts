d9b1b3b20938233b4d63e8922cf221d029d9be40
Eduardo Gonzalez <eduardo_gonzalez@homedepot.com>
2019-02-13 21:10:27 -0500

    Lab5

--- lab4/mean-test/src/app/app.component.ts
+++ lab4/mean-test/src/app/app.component.ts
@@ -8,4 +8,12 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'mean-test';
   today: number = Date.now();
+  powers = ['Really Smart', 'Super Flexible',
+            'Super Hot', 'Weather Changer'];
+  // model = new Hero(18, 'Dr IQ', this.powers[0], 'Chuck Overstreet');
+  submitted = false;
+  onSubmit() { this.submitted = true; }
+  // get diagnostic() { return JSON.stringify(this.model); }
 }
+
+model = new Hero(18, 'Dr IQ', this.powers[0], 'Chuck Overstreet');
